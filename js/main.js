c0eb1b9ac31cae75e612a09e318b5dad4ed0f319
WestSopho <153777405+WestSopho@users.noreply.github.com>
2024-02-07 10:43:50 -0500

    final commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 var heroImg = document.getElementById("image-hero");
+var heroWrapper = document.getElementById("image-hero-wrapper");
 
 function adjustHero(){
     if(window.innerWidth >= 640){
         heroImg.style.top = -heroImg.clientHeight/4 + "px";
-        heroImg.style.right = -heroImg.clientWidth/2.8 + "px";
+        heroImg.style.right = -heroImg.clientWidth/2.5 + "px";
+        heroWrapper.style.marginLeft = -heroImg.clientWidth/2.5 + "px";
     }
-    else{
+    else if(window.innerWidth < 640){
         heroImg.style.top = 0;
         heroImg.style.right = 0;
+        heroWrapper.style.marginLeft = 0;
     }
 
 }
